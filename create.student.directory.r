3fdfdde3691ea4e420a2aa049d051b990c1d4c3c
Robert Leaf <34246014+rtleaf@users.noreply.github.com>
2020-01-22 13:54:09 -0600

    final commit for 2020 spring course

---
+++ create.student.directory.r
@@ -0,0 +1,21 @@
+# Create student upload directories
+# Robert Leaf 
+# Jan. 21, 2020
+
+student.list <- read.csv("./Students 2020/student.list.csv")
+
+student.path <- paste0("./Students 2020/", 
+                       as.character(student.list$LAST.CODE))
+
+for (j in 1:length(student.path)) {
+dir.create(student.path[j]) }
+
+for (j in 1:length(student.path)) {
+  for (k in 1:4) {
+  dir.create(paste0(student.path[j],"/Assignment ", sprintf("%02d", seq(1,4)[k]))) }
+  dir.create(paste0(student.path[j],"/Project")) 
+}
+
+
+  for (k in 1:length(c(3,5,7,27,9,10,11,12,13,14,15,16,17,18,19,20,21))) {
+    dir.create(paste0(student.path[j],"/R4DS ", sprintf("%02d", c(3,5,7,27,9,10,11,12,13,14,15,16,17,18,19,20,21)[k]))) }
